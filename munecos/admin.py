02601df3e3255909842e4463432738504ceb4955
GaelGC04 <garciajorgecf@gmail.com>
2023-11-29 16:00:44 -0600

    Añadiendo modelos a munecos/admin.py

--- munecos/admin.py
+++ munecos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Categoria, Muneco, Tamano, Opinion
 
-# Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Muneco)
+admin.site.register(Tamano)
+admin.site.register(Opinion)
